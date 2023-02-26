c5b3b27b6b1fa367b46d4f80e4b3a2b2ee174f24
vhqr0 <zq_cmd@163.com>
2023-02-26 16:32:30 +0800

    port scanner filtered state

--- viscan/port/scanners.py
+++ viscan/port/scanners.py
@@ -31,11 +31,11 @@ class PortScanner(GenericMainMixin, FinalResultMixin[List[Tuple[str, int,
     @override(PcapScanMixin)
     def get_pkts(self) -> List[sp.IPv6]:
         pkts = []
-        for target in self.targets:
+        for seq, target in enumerate(self.targets):
             pkt = sp.IPv6(dst=target[0]) / \
                 sp.TCP(sport=self.port,
                        dport=target[1],
-                       seq=random.getrandbits(32),
+                       seq=seq,
                        flags='S',
                        window=1024,
                        options=[('MSS', 1460)])
@@ -48,18 +48,20 @@ class PortScanner(GenericMainMixin, FinalResultMixin[List[Tuple[str, int,
 
     @override(FinalResultMixin)
     def parse(self):
-        results = []
+        results = [(addr, port, 'filtered') for addr, port in self.targets]
         for result in self.results:
             try:
                 ippkt = sp.Ether(result)[sp.IPv6]
                 tcppkt = ippkt[sp.TCP]
-                flags = tcppkt.flags
-                if 'R' in flags:
-                    results.append((ippkt.src, tcppkt.sport, 'closed'))
-                elif 'S' in flags and 'A' in flags:
-                    results.append((ippkt.src, tcppkt.sport, 'open'))
-                else:
-                    raise ValueError('invalid tcp flags')
+                seq = tcppkt.ack - 1
+                if seq <= len(results) and \
+                   ippkt.src == results[seq][0] and \
+                   tcppkt.sport == results[seq][1]:
+                    flags = tcppkt.flags
+                    if 'R' in flags:
+                        results[seq] = (ippkt.src, tcppkt.sport, 'closed')
+                    elif 'S' in flags and 'A' in flags:
+                        results[seq] = (ippkt.src, tcppkt.sport, 'open')
             except Exception as e:
                 self.logger.warning('except while parsing: %s', e)
         self.final_result = results
