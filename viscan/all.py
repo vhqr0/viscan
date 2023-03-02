e18363dcfb871e7d51da048e60977f193d968a82
vhqr0 <zq_cmd@163.com>
2023-03-02 09:58:13 +0800

    rename * => *scan

--- viscan/all.py
+++ viscan/all.py
@@ -1,6 +1,6 @@
 # flake8: noqa
 
 from .traceroute import RouteTracer
-from .host import HostScanner
-from .port import PortScanner
-from .dns import DNSScanner
+from .hostscan import HostScanner
+from .portscan import PortScanner
+from .dnsscan import DNSScanner
