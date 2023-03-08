916e69a908c836ed1b35bcce31ed5c3944440f95
vhqr0 <zq_cmd@163.com>
2023-03-08 19:11:09 +0800

    dns route tracer fix init args

--- viscan/traceroute/dns.py
+++ viscan/traceroute/dns.py
@@ -2,9 +2,13 @@ import scapy.layers.inet as inet
 import scapy.layers.inet6 as inet6
 import scapy.layers.dns as dns
 
+from typing import Any
+from argparse import Namespace
+
 from ..common.base import MainRunner
 from ..common.pcap import PcapScanner
 from ..common.decorators import override, auto_add_logger
+from ..common.generators import AddrGenerator
 from .base import RouteSubTracer, RouteTracer
 
 
@@ -19,6 +23,16 @@ class DNSRouteSubTracer(RouteSubTracer, PcapScanner):
         ' icmp6[icmp6type]==icmp6-timeexceeded or ' \
         ' (udp dst port {} and udp src port {} and ip6 src {})' \
         ')'
+
+    def __init__(self,
+                 target: str,
+                 target_port: int = 53,
+                 target_name: str = 'www.google.com',
+                 **kwargs):
+        super().__init__(**kwargs)
+        self.target = target
+        self.target_port = target_port
+        self.target_name = target_name
 
     @override(RouteSubTracer)
     def parse(self):
@@ -45,6 +59,17 @@ class DNSRouteSubTracer(RouteSubTracer, PcapScanner):
             inet.UDP(sport=self.port, dport=self.target_port) / \
             dns.DNS(qd=dns.DNSQR(qname=self.target_name, qtype='AAAA'))
         return pkt
+
+    @classmethod
+    @override(MainRunner)
+    def parse_args(cls, args: Namespace) -> dict[str, Any]:
+        kwargs = super().parse_args(args)
+        kwargs['target'] = AddrGenerator.resolve(args.targets[0])
+        kwargs['name'] = args.targets[1] \
+            if len(args.targets) >= 2 else 'www.google.com'
+        kwargs['port'] = int(args.targets[2]) \
+            if len(args.targets) >= 3 else 53
+        return kwargs
 
 
 @auto_add_logger
